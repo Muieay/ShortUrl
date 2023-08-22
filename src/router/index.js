d4447acf57218d665ba3a94651629057a8b00f99
Muieay <muieay@qq.com>
2023-08-22 12:23:39 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HelloWorld from '../components/HelloWorld.vue'
 import ShowWeb from '../components/ShowWeb.vue'
 const routes = [
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
